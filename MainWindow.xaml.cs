7e277b6fce6702af0652695f4088302f10c54728
flyrys <70770099+flyrys@users.noreply.github.com>
2020-09-27 14:40:44 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+
+namespace Test1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        MyData myData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myData = new MyData("https://jsonplaceholder.typicode.com/posts");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (myData.Read() == 1)
+            {
+                MessageBox.Show("Произошла ошибка при обращении к сайту.\n" +
+                    "Обратитесь к системному администратору.", "Ошибка");
+            }
+            else
+            {
+                myListBox.ItemsSource = myData.book;
+                userListBox.ItemsSource = myData.userId;
+                if (myData.userId.Count != 0)
+                {
+                    userListBox.SelectedItem = userListBox.Items[0];
+                }
+             }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (myData.userId.Count != 0)
+            {
+
+                Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+                ExcelApp.Application.Workbooks.Add(Type.Missing);
+                ExcelApp.Columns.ColumnWidth = 75;
+
+                int i = 1;
+                foreach (Record element in myData.book)
+                {
+                    if (element.userId == myData.userId[userListBox.SelectedIndex])
+                    {
+                        ExcelApp.Cells[i, 1] = element.title;
+                        ExcelApp.Cells[i + 1, 1] = element.body;
+
+                        ExcelApp.Range["A" + i.ToString(), "A" + i.ToString()].HorizontalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+                        ExcelApp.Range["A" + i.ToString(), "A" + (i + 1).ToString()].Borders.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+
+                        i = i + 3;
+                    }
+                }
+                ExcelApp.Visible = true;
+            }
+        }
+    }
+}
---
+++ MyData.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+
+namespace Test1
+{
+    class Record
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        public override string ToString()
+        {
+            return userId.ToString() + " " + id.ToString() + " " + title + " " + body;
+        }
+
+
+    }
+
+    class MyData
+    {
+        private string siteName;        // Ссылка на сайт
+        private WebClient webClient;    // webClient
+        public List<Record> book;       // Список публикаций
+        public List<int> userId;        // Список пользователей
+
+
+        // Конструктор
+        public MyData(string pSiteName)
+        {
+            siteName = pSiteName;
+            webClient = new WebClient();
+            userId = new List<int>();
+        }
+
+        // Конструктор
+        public MyData()
+        {
+            siteName = "";
+            webClient = new WebClient();
+        }
+
+        // Установить ссылку на сайт
+        public void SetSiteName(string pSiteName)
+        {
+            siteName = pSiteName;
+        }
+
+        // Прочитать сайт Get запросом
+        public int Read()
+        {
+            int error = 0;
+            try
+            {
+                string result = webClient.DownloadString(siteName);
+                book = JsonSerializer.Deserialize<List<Record>>(result);
+                foreach (Record element in book)
+                {
+                    if (userId.IndexOf(element.userId) == -1)
+                    {
+                        userId.Add(element.userId);
+                    }
+
+                }
+            }
+            catch
+            {
+                error = 1;
+            }
+            return error;
+        }
+
+    }
+}
